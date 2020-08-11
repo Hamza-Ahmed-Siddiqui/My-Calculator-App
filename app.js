a7b4897647c6f6b40fa041d2d5918632c9991bb0
Hamza-Ahmed-Siddiqui <princehamzaahmed9@gmail.com>
2020-08-12 01:58:24 +0500

    My Calculator App

---
+++ app.js
@@ -0,0 +1,19 @@
+function getNumber(num){
+    var result=document.getElementById("res")
+         result.value +=num
+
+    
+    
+}
+function clearResult(){
+    var result=document.getElementById("res")
+    result.value = ""
+}
+function getresult(){
+    var result=document.getElementById("res")
+    result.value =eval(result.value)
+}
+ 
+
+  
+ 
